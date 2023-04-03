8b1be334c590d0ea489a940dfce389346f0fdba1
Aranxa Codina <tassa780@gmail.com>
2023-04-03 12:06:56 +0200

    use dependency injector and use swagger

---
+++ FinishLine/Bootstrap/DependencyInjector.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace FinishLine.Api.Bootstrap
+{
+    public class DependencyInjector
+    {
+        public static void InjectRepositories(IServiceCollection services)
+        {
+            
+        }
+
+        public static void InjectServices(IServiceCollection services)
+        {
+           
+        }
+    }
+}
---
+++ FinishLine/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinishLine.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok("Je suis un controller qui marche");
+        }
+    }
+}
